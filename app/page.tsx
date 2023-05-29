7fe02d4616984a28fed7872982ff2d371ae0353f
myzsk <myzskdev@gmail.com>
2023-05-29 15:46:20 +0700

    add list item

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import Image from 'next/image'
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListing";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>airbnb</div>
-      )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                currentUser={currentUser}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
