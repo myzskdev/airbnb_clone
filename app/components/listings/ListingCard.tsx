3c460728b89eef96e7ea03719aafc68d88944b06
myzsk <myzskdev@gmail.com>
2023-05-29 17:15:23 +0700

    add feature to favorite item of list

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import useCountries from "@/app/hooks/useCountry";
-import { SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
@@ -11,7 +11,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
@@ -22,12 +22,12 @@ interface ListingCardProps {
 
 const ListingCard: React.FC<ListingCardProps> = ({
   data,
-  actionId = "",
-  actionLabel,
-  currentUser,
-  disabled,
-  onAction,
   reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = "",
+  currentUser,
 }) => {
   const router = useRouter();
   const { getByValue } = useCountries();
